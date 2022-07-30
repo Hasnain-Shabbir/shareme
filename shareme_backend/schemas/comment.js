648d12a304ccbc5d23197c90233e848b1e249f0b
Hasnain-Shabbir <hasnainshabbir186@gmail.com>
2022-07-31 03:22:11 +0500

    Setup the Shareme Backend, and setup Tailwind for frontend.

---
+++ shareme_backend/schemas/comment.js
@@ -0,0 +1,17 @@
+export default {
+  name: 'comment',
+  type: 'Comment',
+  type: 'document',
+  fields: [
+    {
+      name: 'postedBy',
+      title: 'PostedBy',
+      type: 'postedBy',
+    },
+    {
+      name: 'comment',
+      title: 'Comment',
+      type: 'string',
+    },
+  ],
+};
