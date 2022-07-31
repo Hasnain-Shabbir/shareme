55ba7ffff60746324d3172a76bff724cb72d5503
Hasnain-Shabbir <hasnainshabbir186@gmail.com>
2022-08-01 01:09:57 +0500

    Setup the Routing.

--- shareme_frontend/src/App.js
+++ shareme_frontend/src/App.js
@@ -1,11 +1,14 @@
-
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import { Login } from './components/components';
+import { Home } from './container/container';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Shareme
-    </div>
+    <Routes>
+      <Route path='login' element={<Login />} />
+      <Route path='/*' element={<Home />} />
+    </Routes>
   );
 }
 
